7842c056a211010e1a0fbd3c33e16ca3ed082db6
Mateusz Wende <matewen129@student.polsl.pl>
2020-04-14 13:35:21 +0200

    Fix the condition

--- src/hoc/withOnlyMobileAndTablet.tsx
+++ src/hoc/withOnlyMobileAndTablet.tsx
@@ -4,7 +4,7 @@ import { isMobile, isTablet } from "react-device-detect";
 const withOnlyMobileAndTablet = <P extends object>(
   Component: React.ComponentType<P>
 ): React.FC<P> => ({ ...props }) =>
-  isMobile && isTablet ? (
+  isMobile || isTablet ? (
     <Component {...(props as P)} />
   ) : (
     <p>App is only available on mobile and tablets.</p>
